47b37bf22f40c180187bc354bce9ccdfe2ae7979
Praashna Adhikari <prashnaadhikari02@gmail.com>
2024-10-07 14:17:56 +0545

    Update SeleniumTest.java

--- SeleniumTest.java
+++ SeleniumTest.java
@@ -1,28 +1,51 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
-import java.sql.Driver;
+import java.sql.SQLException;
 
 public class SeleniumTest {
-   public  static  ChromeOptions options;
-public static WebDriver  driver;
+
+    public static ChromeOptions options;
+    public static WebDriver driver ;
 
     @BeforeTest
-   public static void setup(){
-
-        options=new ChromeOptions();
+    public static void Setup()  {
+        options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-        System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver.exe");
-        driver =new ChromeDriver(options);
-        driver.get("https://english.onlinekhabar.com/90s-popular-candies-childhood.html ");
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver(options);
+
+        driver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html");
     }
+
     @Test
-    void test(){
-        driver.findElement(By.xpath("//*[@id=\"page\"]/header/div[1]/div/div[3]")).click();
-        driver.findElement(By.xpath("//*[@id=\"primary-menu\"]/li[4]/a")).click();
+    void testsuite() throws InterruptedException, SQLException {
+        driver.findElement(By.xpath("//*[@id=\"menuToggle\"]/input")).click(); //for clicking the element
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"menu\"]/a[2]/li")).click(); //xpath is where element is exactly placed
+
+        driver.findElement(By.xpath("//*[@id=\"usr\"]")).sendKeys("sa"); //a input field so using sendkeys
+        driver.findElement(By.xpath("//*[@id=\"pwd\"]")).sendKeys("sa");
+
+        driver.findElement(By.xpath("//*[@id=\"second_form\"]/input")).click();
+
+        WebElement webElement = driver.findElement(By.xpath("//*[@id=\"ShoeType\"]"));
+
+        String actualFirstCategory = webElement.getText();  //gets xpath and store in string
+
+        String expectedFirstCategory = "Formal Shoes";
+
+        Assert.assertEquals(expectedFirstCategory, actualFirstCategory );  //asserts are validation or checkpoints for an application
+        driver.close();
+
+
     }
+
 }
